eaf55f2c58d665794025f7690ea14e252c73594d
TokuhiroNoboru <tokujobn@gmail.com>
2021-03-31 09:59:48 +0900

    s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,8 +10,8 @@ CarrierWave.configure do |config|
     config.fog_public = false
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['S3_ACCESS_KEY_ID'], # アクセスキー
-      aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'], # シークレットアクセスキー
+      aws_access_key_id: 'AKIARDWG3XUQ2CFHO43A', # アクセスキー
+      aws_secret_access_key: '4gLXDBXyjFR5LyLRDlzCFf47NWyby404jvcABYCk', # シークレットアクセスキー
       region: 'ap-northeast-1', # リージョン
       path_style: true
     }
